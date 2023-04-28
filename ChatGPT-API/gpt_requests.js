306f350f446d85810c7949d29d7a5c35ba1dfbf3
Kiaran Lau <76144488+pluffpenguin@users.noreply.github.com>
2023-04-28 11:25:41 -0700

    started chatgpt

---
+++ ChatGPT-API/gpt_requests.js
@@ -0,0 +1,6 @@
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.listModels();
