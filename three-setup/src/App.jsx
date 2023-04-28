1db7167f2cb69ba25b6b0c52fc18692de7f889d3
Rafa <rafumojica@gmail.com>
2023-04-28 13:09:29 -0700

    fixed folder structure

---
+++ three-setup/src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import * as THREE from "three";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    const scene = new THREE.Scene();
+
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.z = 96;
+
+    const canvas = document.getElementById("myThreeJsCanvas");
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true,
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    ambientLight.castShadow = true;
+    scene.add(ambientLight);
+
+    const spotLight = new THREE.SpotLight(0xffffff, 1);
+    spotLight.castShadow = true;
+    spotLight.position.set(0, 64, 32);
+    scene.add(spotLight);
+
+    const animate = () => {
+      renderer.render(scene, camera);
+      window.requestAnimationFrame(animate);
+    };
+    animate();
+  }, []);
+
+  return (
+    <>
+      <canvas id="myThreeJsCanvas" />
+      <h1>Hello world</h1>
+    </>
+  );
+}
+
+export default App;
