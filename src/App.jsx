db9d55875d548bc0311f77dd68b9f54a17f2f204
Rafa <rafumojica@gmail.com>
2023-04-29 11:33:03 -0700

    cleaned up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
-import * as THREE from "three";
-
 import "./styles/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
 import Home from "./pages/Home";
-
-import { setup_renderer } from "./Classes/Settings.jsx"
-
-// import CameraClass from "./Classes/Camera.jsx";
-import PlayerClass from "./Classes/Player.jsx";
 
 function App() {
 
