75377f93efd38101132b34fd2018c648db5ee5dd
tvpower89 <tvpower89@hotmail.com>
2023-04-29 16:08:27 -0700

    classes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -113,7 +113,11 @@ function Home() {
 
     //nuclear plant model
     let nuclearModel = new Model3D('src/assets/models/scene.gltf', scene, 10, -28, -1.2, 85);
+
+    //wind turbine
     let windTurbine = new Model3D('src/assets/models/windTurbine.gltf', scene,10,0, -2);
+
+    //solar panels
     for (let i = 0; i < 5; i++) {
       new Model3D('src/assets/models/soler_panel_setup/scene.gltf', scene, 10, 52 ,0,20 + (i*6))
     }
