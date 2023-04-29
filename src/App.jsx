6de0f8a51adc349d8e5e168cac01adb17753daf0
tvpower89 <tvpower89@hotmail.com>
2023-04-28 21:43:29 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {useEffect} from "react";
 import * as THREE from "three";
 import "./App.css";
 
@@ -22,7 +22,25 @@ function App() {
     camera.rotation.x = -.2;
     camera.rotation.y = .8;
     camera.rotation.z = 0.556;
-    
+
+    const scene2 = new THREE.Scene();
+    const cam2 = new THREE.PerspectiveCamera(
+        90,
+        1,
+        0.1,
+        1000
+    )
+    cam2.position.z = 30;
+    const canvas2 = document.getElementById("myThreeJsCanvas2")
+    const canvas2_container = document.getElementById("canvas2-container")
+    const renderer2 = new THREE.WebGLRenderer({
+      canvas2,
+      antialias: true,
+    });
+    renderer2.setSize(250, 250);
+    canvas2_container.appendChild(renderer2.domElement)
+
+
 
     const canvas = document.getElementById("myThreeJsCanvas");
     const renderer = new THREE.WebGLRenderer({
@@ -41,10 +59,15 @@ function App() {
     spotLight.position.set(0, 64, 32);
     scene.add(spotLight);
 
-    const baseplateGeometry = new THREE.baseplateGeometry(100, 1, 100);
+    const baseplateGeometry = new THREE.BoxGeometry(100, 1, 100);
     const baseplateMaterial = new THREE.MeshNormalMaterial();
     const baseplateMesh = new THREE.Mesh(baseplateGeometry, baseplateMaterial);
     scene.add(baseplateMesh);
+
+    const sphere = new THREE.SphereGeometry(16,16,16);
+    const sphereMat = new THREE.MeshNormalMaterial();
+    const sphereMesh = new THREE.Mesh(sphere, sphereMat);
+    scene2.add(sphereMesh);
 
     baseplateMesh.rotation.x = .6
 
@@ -52,6 +75,7 @@ function App() {
       // baseplateMesh.rotation.x += 0.01;
       // baseplateMesh.rotation.y += 0.01;
       renderer.render(scene, camera);
+      renderer2.render(scene2,cam2)
       window.requestAnimationFrame(animate);
     };
     animate();
@@ -59,6 +83,7 @@ function App() {
 
   return (
     <>
+      <div id="canvas2-container"></div>
       <canvas id="myThreeJsCanvas" />
     </>
   );
