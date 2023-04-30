d6c087139c7e22147039549c6f7e79a3fcff8f5b
Rafa <rafumojica@gmail.com>
2023-04-29 17:02:45 -0700

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ import {OBJLoader} from "three/addons/loaders/OBJLoader.js";
 import { VOXLoader} from "three/addons/loaders/VOXLoader.js";
 import { GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 
-import Model3D from "../Classes/importModels.jsx";
+import Model3D from "../Classes/importModels.js";
 
 import { TestBuilding } from "../Classes/testBuilding";
 
